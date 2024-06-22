74b19761543c21f89fe0984a0e40b26dd6c0aa9c
neil tarar <neiltarar@gmail.com>
2024-06-22 21:44:52 +1000

    deploy

--- movies-app-webclient/src/api/movie-api.js
+++ movies-app-webclient/src/api/movie-api.js
@@ -1,14 +1,14 @@
 export const fetchMovies = async (searchQuery) => {
-  console.log("searchQuery = ", searchQuery);
-const url = `http://localhost:3001/movies/${searchQuery}`
-const response = await fetch(url)
-const data = await response.json()
-return data
-}
+	console.log("searchQuery = ", searchQuery);
+	const url = `/movies/${searchQuery}`;
+	const response = await fetch(url);
+	const data = await response.json();
+	return data;
+};
 
 export const fetchMovieDetails = async (imdbID) => {
-  const url = `http://localhost:3001/movies/id/${imdbID}`
-  const response = await fetch(url)
-  const data = await response.json()
-  return data
-}
+	const url = `/movies/id/${imdbID}`;
+	const response = await fetch(url);
+	const data = await response.json();
+	return data;
+};
