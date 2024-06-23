ca4569308d39a37a2fb1444bd4233e2a06257f74
neil tarar <neiltarar@gmail.com>
2024-06-23 12:26:07 +1000

    adds the prod url to webclient

--- movies-app-webclient/src/api/movie-api.js
+++ movies-app-webclient/src/api/movie-api.js
@@ -1,13 +1,13 @@
 export const fetchMovies = async (searchQuery) => {
 	console.log("searchQuery = ", searchQuery);
-	const url = `/movies/${searchQuery}`;
+	const url = `https://david-lesson.onrender.com/movies/${searchQuery}`;
 	const response = await fetch(url);
 	const data = await response.json();
 	return data;
 };
 
 export const fetchMovieDetails = async (imdbID) => {
-	const url = `/movies/id/${imdbID}`;
+	const url = `https://david-lesson.onrender.com/movies/id/${imdbID}`;
 	const response = await fetch(url);
 	const data = await response.json();
 	return data;
